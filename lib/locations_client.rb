f6242f2fdcb6afcd4995d2b8987b7b7f6795150b
Rob Hesketh <rhesketh.uk@gmail.com>
2019-03-14 21:06:51 +0000

    Added LocationsClient for submitting locations of found suspects to the API

---
+++ lib/locations_client.rb
@@ -0,0 +1,31 @@
+require 'http'
+
+# Contacts a remote service to send cat location information
+class LocationsClient
+  HOST = 'http://which-technical-exercise.herokuapp.com/api'.freeze
+  ENDPOINT = 'location'.freeze
+
+  def initialize(http_client: HTTP)
+    @http_client = http_client
+  end
+
+  def submit(x:, y:) # rubocop:disable Naming/UncommunicativeMethodParamName
+    response = request(x, y)
+    raise IncorrectSubmissionError unless response.code == 200 # OK
+
+    true
+  end
+
+  private
+
+  def request(x, y) # rubocop:disable Naming/UncommunicativeMethodParamName
+    url = url_for(x, y)
+    @http_client.get(url)
+  end
+
+  def url_for(x, y) # rubocop:disable Naming/UncommunicativeMethodParamName
+    "#{HOST}/#{ENV['ENDPOINT_EMAIL']}/#{ENDPOINT}/#{x}/#{y}"
+  end
+
+  class IncorrectSubmissionError < StandardError; end
+end
---
+++ spec/locations_client_spec.rb
@@ -0,0 +1,38 @@
+require './lib/locations_client.rb'
+
+RSpec.describe LocationsClient, type: :model do
+  describe '#submit' do
+    it 'Makes a call to a remote service based on the provided coordinates' do
+      response = double(:response, code: 200)
+      http_client = class_double(HTTP, get: response)
+      subject = described_class.new(http_client: http_client)
+
+      expected_url = "#{LocationsClient::HOST}/#{ENV['ENDPOINT_EMAIL']}/#{LocationsClient::ENDPOINT}/5/7"
+      expect(http_client).to receive(:get).with(expected_url)
+
+      subject.submit(x: 5, y: 7)
+    end
+
+    it 'returns true if the response was successfully received' do
+      response = double(:response, code: 200)
+      http_client = class_double(HTTP, get: response)
+      subject = described_class.new(http_client: http_client)
+
+      expect(subject.submit(x: 0, y: 0)).to eq true
+    end
+
+    context 'Error handling' do
+      # NOTE: This functionality may change. I have assumed that a successful
+      # submission will return a 200 OK whereas an unsuccessful submission
+      # will return an error status. I have no documentation nor was I able to
+      # get an error response from the endpoint that verifies this assumption.
+      it 'Raises an error if the response was not 200 OK' do
+        response = double(:response, code: 500)
+        http_client = class_double(HTTP, get: response)
+        subject = described_class.new(http_client: http_client)
+
+        expect { subject.submit(x: 0, y: 0) }.to raise_error(LocationsClient::IncorrectSubmissionError)
+      end
+    end
+  end
+end
